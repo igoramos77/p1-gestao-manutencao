884c3333c180c6517c3c67626d1559d8c75086f9
igoramos77 <ibrownramos@gmail.com>
2022-04-05 20:42:16 -0300

    fix: code reorganization

--- prova.py
+++ prova.py
@@ -8,6 +8,9 @@ class Player:
 
         def decrease_points(self):
             self.points -= 1
+        
+        def get_player_points(self):
+            self.points
 
 
 class Game:
@@ -17,29 +20,39 @@ class Game:
 
     def score(self):
         result = ""
+        
         if (self.player1.points == self.player2.poits and self.player1.points < 3):
             if (self.player1.points == 0):
                 result = "Love"
+                
             if (self.player1.points == 1):
                 result = "Fifteen"
+                
             if (self.player1.points == 2):
                 result = "Thirty"
+                
             result += "-All"
+            
         if (self.player1.points == self.player2.points and self.player1.points > 2):
             result = "Deuce"
 
         P1res = ""
         P2res = ""
+        
         if (self.player1.points > 0 and self.player2.points == 0):
             if (self.player1.points == 1):
                 P1res = "Fifteen"
+                
             if (self.player1.points == 2):
                 P1res = "Thirty"
+                
             if (self.player1.points == 3):
                 P1res = "Forty"
 
             P2res = "Love"
+            
             result = P1res + "-" + P2res
+            
         if (self.player2.points > 0 and self.player1.points == 0):
             if (self.player2.points == 1):
                 P2res = "Fifteen"
@@ -54,36 +67,47 @@ class Game:
         if (self.player1.points > self.player2.points and self.player1.points < 4):
             if (self.player1.points == 2):
                 P1res = "Thirty"
+                
             if (self.player1.points == 3):
                 P1res = "Forty"
+                
             if (self.player2.points == 1):
                 P2res = "Fifteen"
+                
             if (self.player2.points == 2):
                 P2res = "Thirty"
+                
             result = P1res + "-" + P2res
+            
         if (self.player2.points > self.player1.points and self.player2.points < 4):
             if (self.player2.points == 2):
                 P2res = "Thirty"
+                
             if (self.player2.points == 3):
                 P2res = "Forty"
+                
             if (self.player1.points == 1):
                 P1res = "Fifteen"
+                
             if (self.player1.points == 2):
                 P1res = "Thirty"
+                
             result = P1res + "-" + P2res
 
         if (self.player1.points > self.player2.points and self.player2.points >= 3):
-            result = "Advantage " + self.player1Name
+            result = "Advantage " + self.player1.name
 
         if (self.player2.points > self.player1.points and self.player1.points >= 3):
-            result = "Advantage " + self.player2Name
+            result = "Advantage " + self.player2.name
 
         if (self.player1.points >= 4 and self.player2.points >= 0 and (
                 self.player1.points - self.player2.points) >= 2):
-            result = "Win for " + self.player1Name
+            result = "Win for " + self.player1.name
+            
         if (self.player2.points >= 4 and self.player1.points >= 0 and (
                 self.player2.points - self.player1.points) >= 2):
-            result = "Win for " + self.player2Name
+            result = "Win for " + self.player2.name
+            
         return result
 
     def SetP1Score(self, number):
@@ -101,6 +125,7 @@ class Game:
         self.player2.points += 1
 
 
+### Client
 p1 = Player("Igor", 0)
 p2 = Player("Tassio", 0)
 
